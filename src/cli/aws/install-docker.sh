2316c1855d4b2ae0b1b13b4900ba228fc1ae3de2
Chris Mahoney <44449504+chrimaho@users.noreply.github.com>
2022-04-20 07:44:38 +1000

    Create install-docker.sh

---
+++ src/cli/aws/install-docker.sh
@@ -0,0 +1,38 @@
+# Uninstall old versions
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# Update the package index
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Set up the stable repository
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+	  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker Engine
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# Verify that Docker Engine is installed correctly
+sudo docker run hello-world
+
+# Download the stable release of Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Apply executable permissions to the binary
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Test the installation
+docker-compose --version
+
+# Update user so docker can run without needing the sudo command
+sudo groupadd docker
+sudo usermod -aG docker $USER
